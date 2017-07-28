0b161d610f636c33bf075738482dbf56c980314b
Mark Walker <mwalkerpeps@gmail.com>
2017-07-28 17:20:20 -0400

    Initial Commit

---
+++ swiftc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swiftc
+//
+//  Created by Mark Walker on 7/28/17.
+//  Copyright © 2017 Mark Walker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
